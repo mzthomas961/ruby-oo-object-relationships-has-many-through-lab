e56d4234b6799989e35abb8d4c6682146877d88f
Matthew Thomas <mzthomas96@gmail.com>
2021-01-19 20:17:26 -0500

    commit tree 4cc606ffb6c260d0b12809328b2789bdaa79258c

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,12 @@
+class Doctor
+    @@all = []
+    attr_accessor :name
 
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+end
